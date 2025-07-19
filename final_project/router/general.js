9a9209ccec5b829057fd89db565572f6a424d5f2
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-19 13:41:47 +0530

    Implement GET functionality to retrieve books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,8 +26,15 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	const author = req.params.author;
+	let foundBooks = Object.values(books).filter(
+		(book) => book.author.toLowerCase() === author.toLowerCase()
+	);
+	if (foundBooks.length > 0) {
+		return res.status(200).json(foundBooks);
+	} else {
+		return res.status(404).json({ message: "No books found by this author" });
+	}
 });
 
 // Get all books based on title
