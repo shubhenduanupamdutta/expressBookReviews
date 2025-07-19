b0b86c7f823ea2262d6330211482e8c7e71933eb
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-19 14:18:13 +0530

    Implement user registration functionality with validation

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,15 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	const { username, password } = req.body;
+	if (!username || !password) {
+		return res.status(400).json({ message: "Username and password are required" });
+	}
+	if (isValid(username)) {
+		return res.status(400).json({ message: "Username already exists. Please login." });
+	}
+	users.push({ username, password });
+	return res.status(201).json({ message: "User registered successfully" });
 });
 
 // GET: Get the book list available in the shop
