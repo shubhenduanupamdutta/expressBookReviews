19b664a60c1f7fe8ddc0aac76cfe6aaf167e54e0
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-19 14:07:33 +0530

    Implement GET functionality to retrieve books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,8 +39,15 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	const title = req.params.title;
+	let foundBooks = Object.values(books).filter(
+		(book) => book.title.toLowerCase() === title.toLowerCase()
+	);
+	if (foundBooks.length > 0) {
+		return res.status(200).json(foundBooks);
+	} else {
+		return res.status(404).json({ message: "No books found with this title" });
+	}
 });
 
 //  Get book review
