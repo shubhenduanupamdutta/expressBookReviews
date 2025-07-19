b049b7ddf81c4ffe0977a05e59ba00bb51a2c789
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-19 13:22:56 +0530

    Improve authentication mechanism and fix comment formatting

--- final_project/index.js
+++ final_project/index.js
@@ -14,7 +14,20 @@ app.use(
 );
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-	//Write the authenication mechanism here
+	// Write the authentication mechanism here
+	if (req.session && req.session.authorization) {
+		let token = req.session.authorization["accessToken"];
+		jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
+			if (!err) {
+				req.user = user;
+				next();
+			} else {
+				return res.status(403).send("User not authenticated");
+			}
+		});
+	} else {
+		return res.status(403).send("User not logged in");
+	}
 });
 
 const PORT = 5000;
