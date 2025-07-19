e2c3396258e539c5bdc9685e7ed15f330ad7e8c6
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-19 14:25:20 +0530

    Implement login functionality with validation and JWT token generation

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -18,8 +18,21 @@ const authenticatedUser = (username, password) => {
 
 //POST: only registered users can login
 regd_users.post("/login", (req, res) => {
-	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	const { username, password } = req.body;
+	if (!username || !password) {
+		return res
+			.status(400)
+			.json({ message: "Error logging in. Username and password are required" });
+	}
+	if (authenticatedUser(username, password)) {
+		let accessToken = jwt.sign({ data: username }, process.env.SECRET_TOKEN, {
+			expiresIn: 60 * 60,
+		});
+		req.session.authorization = { accessToken, username };
+		return res.status(200).json({ message: "User successfully logged in", accessToken });
+	} else {
+		return res.status(401).json({ message: "Invalid login. Check username and password." });
+	}
 });
 
 // Add a book review
